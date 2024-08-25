fe64b23f6dd8abe17d3303f00fccecb5fb2bf6f2
Hashir Asad <hashirbinasad@gmail.com>
2024-08-25 23:32:57 +0500

    .

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+
+
+fn main() {
+    println!("Hey , lets guess a number");
+
+    let secret_number = rand::thread_rng().gen_range(0..100);
+
+    println!("The secret number is {}", secret_number);
+
+    println!("Input your guess");
+    let mut guess: String =  String::new();
+    io::stdin().read_line(&mut guess).expect("Error reading line");
+
+    let guess: u32 = guess.trim().parse().expect("Error"); 
+
+    match guess.cmp(&secret_number) {
+
+        Ordering::Equal => println!("Equal"),
+        Ordering::Greater => println!("Greater"),
+        Ordering::Less => println!("Less"),
+
+
+
+    }
+
+}
+
+
+
+//demo
